eec2acfb31c4e0890b2db067843214f5d11a4a7d
user <g.usatov@gmail.com>
2025-11-17 16:15:35 +0300

    Нашел оптимальный способ транскрибации для 5080

---
+++ agora-backend/src/main/java/ai/agorabackend/AgoraBackendApplication.java
@@ -0,0 +1,13 @@
+package ai.agorabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgoraBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgoraBackendApplication.class, args);
+    }
+
+}
